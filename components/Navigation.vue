e5fbf64351574d45a6f9575dc27824af0ad205bf
Raj-Hunjan <rajhunjann@gmail.com>
2021-12-14 22:43:56 -0700

    more tweaks

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,23 +1,24 @@
 <template>
-  <header>
-    <nav class="nav-container">
-      <div class="logo">
-      <NuxtLink to="/"><svg viewBox="0 0 1211 1750" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <nav class="navbar">
+  <div class="logo">
+    <NuxtLink to="/"><svg viewBox="0 0 1211 1750" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="m0 0 200 95.8V859L0 962V0Z" fill="#E0CA3C"/>
       <path d="m0 758 200 95.8V1617L0 1748.5V758ZM0 221.8 85.5 41l761.7 360.3L761.7 582z" fill="#E0CA3C"/>
       <path fill="#E0CA3C" d="M647 400h200v449H647zM120 1049V849h727v200z"/>
       <path d="M44 961.8 261.7 895l582.7 590.7 2.2 263.5L44 961.8ZM847 1049V849h164v200zM1211 990.5l-200-95.8V131.5L1211 0v990.5Z" fill="#E0CA3C"/>
       <path d="m1211 1748.5-200-142v-717L1211 758v990.5Z" fill="#E0CA3C"/>
-      </svg></NuxtLink>
-      </div>
-    <ul class="nav-links">
-      <li><NuxtLink to="/work">work</NuxtLink></li>
-      <li><NuxtLink to="/about">about</NuxtLink></li>
-      <li><NuxtLink to="/contact">contact</NuxtLink></li>
-    </ul>    
-    </nav>
-  </header>
+      </svg>
+    </NuxtLink>
+    </div>
+  <ul class="navbar-links"> 
+    <li><NuxtLink class="nav-text" to="/work">work</NuxtLink></li>
+    <li><NuxtLink class="nav-text" to="/about">about</NuxtLink></li>
+    <li><NuxtLink class="nav-text" to="/contact">contact</NuxtLink></li>     
+  </ul>
+ </nav>
 </template>
+
+
 
 <style>
 * {
@@ -29,22 +30,31 @@
 .logo {
   width: 50px;
   height: 50px;
+  margin-bottom: 20px;
 }
 
+nav {
+    background-color: #6E44FF;
+    font-family: '',;
+    font-weight: 500;
+    font-size: 20px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px;
+  }
 
-.nav-container {
-  background-color: black;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px;
-}
+  nav ul {
+    list-style-type: none;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+  }
 
-.nav-links {
-list-style-type: none;
-display: flex;
-justify-content: flex-end;
-flex-wrap: wrap;
-gap: 15px;
+.nav-text {
+  text-decoration: none;
+  color: #18020C;
+  font-size: 20px;
 }
 </style>
